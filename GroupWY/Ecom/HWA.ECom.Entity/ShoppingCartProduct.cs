5a05628a49f00a82d009c68b3ef7d8368eb76e53
Jianping-Yin <rockybjchina@gmail.com>
2017-03-19 16:55:09 -0500

    12

---
+++ GroupWY/Ecom/HWA.ECom.Entity/ShoppingCartProduct.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWA.ECom.Entity
+{
+
+    public class ShoppingCartProduct:BaseEntity
+    {
+        public int ShoppingCartId
+        {
+            get; set;
+        }
+        public int ProductId
+        {
+            get; set;
+        }
+        public decimal Quantity
+        {
+            get; set;
+        }
+        public decimal UnitPrice
+        {
+            get; set;
+        }
+        public string UnitOfMeasure     
+        {
+            get; set;
+        }
+        public decimal SubTotal
+        {
+            get; set;
+        }
+    }
+}
